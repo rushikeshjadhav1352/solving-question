9f784c7fafffcd71f6c8d10edb36623359767c17
Rushikesh Jadhav <81899664+rushikeshjadhav1352@users.noreply.github.com>
2022-10-19 01:10:58 +0530

    Time: 1 ms (88.96%), Space: 42.2 MB (76.91%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=0;
+        double k=1;
+        if(n<0){
+            return Math.pow(x,n);
+            //return k/ans;
+        }
+            return Math.pow(x,n);
+        
+        
+    }
+}
